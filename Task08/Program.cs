1b282be33e260353f8b06bb6e0f18c0d7d2c2b90
Николай <nnm_58@mail.ru>
2023-09-19 20:03:11 +0300

    Добавил тему и решение задачи №8

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N) а на выходе 
+// показывает все четные числа от до N
+// Например: 5 -> 2, 4
+//           8 -> 2, 4, 6, 8
+
+using System;
+class HelloWorld {
+  static void Main() {
+    Console.Write("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    
+    int evenNumber = 2;
+    
+    if(number > 1)
+    {
+        while(evenNumber <= number)
+        {
+            Console.Write(evenNumber + " ");
+            evenNumber = evenNumber + 2;
+        }
+    }
+  }
+}
