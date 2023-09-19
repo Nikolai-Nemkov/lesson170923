b5586502b76f74796ab8a85e748886197e0a5ce9
Николай <nnm_58@mail.ru>
2023-09-19 09:25:00 +0300

    Добавил тему и решение задачи №3

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая будет выдавать название
+дня недели по заданному номеру
+3 -> Среда
+5 -> Пятница */
+
+string dayNumber;
+Console.EriteLine("Введите номер дня недели");
+
+if (dayNumber == "1")
+{
+    Consoie.Writeline($"{dayNumber} -> Понедельник");
+}
+else if (dayNumber == "2")
+{
+    Consoie.Writeline($"{dayNumber} -> Вторник");
+}
+else if (dayNumber == "3")
+{
+    Consoie.Writeline($"{dayNumber} -> Среда");
+}
+else if (dayNumber == "4")
+{
+    Consoie.Writeline($"{dayNumber} -> Четверг");
+}
+else if (dayNumber == "5")
+{
+    Consoie.Writeline($"{dayNumber} -> Пятница");
+}
+else if (dayNumber == "6")
+{
+    Consoie.Writeline($"{dayNumber} -> Суббота");
+}
+else if (dayNumber == "7")
+{
+    Consoie.Writeline($"{dayNumber} -> Воскресенье");
+}
+else
+{
+    Consoie.Writeline("Дня недели с таким номером нет");
+}
