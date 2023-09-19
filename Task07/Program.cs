a2396be77b92fb435f1fcac2e6f2a9a3b1c925c4
Николай <nnm_58@mail.ru>
2023-09-19 11:11:43 +0300

    Добавил тему и решение задачи №7

--- Task07/Program.cs
+++ Task07/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//   Напишите программу, которая принимает на вход 
+//   трехзначное число и на выходе показывает
+//   последнюю цифру этого числа.
+//   456 -> 6
+//   782 -> 2
+//   918 -> 8
+
+Console.WriteLine("enter int number:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number <0)
+{
+    number *= -1;
+}
+
+if(number >=100 && number <=999)
+{
+    int lastnumber = number % 10;
+    Console.WriteLine($"{number} -> {lastnumber}");
+}
+else
+{
+    
+     Console.WriteLine("try again error");
+    
+}
